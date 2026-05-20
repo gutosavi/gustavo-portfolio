5e1af1cdce92228d5904fbc36038c7e573aba4ae
Gustavo Savi <gutosavi@hotmail.com>
2026-05-20 15:33:03 -0300

    chore: configura ambiente inicial v.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import './App.css'
 
 function App() {
 
   return (
     <div>
-      <h1 className='text-3xl font-bold underline'>
+      <h1 className='bg-primary-600
+       text-3xl font-bold underline'>
         Hello World!
       </h1>
     </div>
