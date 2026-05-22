ad7dd7e9ea3a28668b32a9cb9794059e96c8e42d
Gustavo Savi <gutosavi@hotmail.com>
2026-05-22 16:07:02 -0300

    feat: add typewriter na hero

---
+++ src/components/Typewriter.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Typewriter = ({ text, speed = 50 }: TypewriterProps) => {
+  const [displayedText, setDisplayedText] = React.useState('');
+
+  React.useEffect(() => {
+    let i = 0;
+    const typingInterval = setInterval(() => {
+      if (i < text.length) {
+        setDisplayedText(prev => prev + text.charAt(i));
+        i++;
+      } else {
+        clearInterval(typingInterval);
+      }
+    }, speed);
+
+    return () => clearInterval(typingInterval);
+  }, [text, speed]);
+
+  return (
+    <h2 className="text-lg font-display laptop:text-lg font-bold leading-6 text-primary-600 mt-4 laptop:mt-7">
+      {displayedText}
+    </h2>
+  )
+}
+
+export default Typewriter
--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,3 +1,8 @@
 type ContainerProps = {
   children: React.ReactNode;
 };
+
+interface TypewriterProps {
+  text: string;
+  speed?: number;
+}
