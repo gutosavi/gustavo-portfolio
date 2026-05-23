96795ead8261079ab691ca517f591f0e50ac4903
Gustavo Savi <gutosavi@hotmail.com>
2026-05-22 21:56:24 -0300

    feat: adiciona novo elemento nos componentes Sobre.tsx e SectionTitle.tsx

--- src/sections/Sobre.tsx
+++ src/sections/Sobre.tsx
@@ -7,7 +7,10 @@ const Sobre = () => {
     <section>
       <Container>
         <div className='layout-grid'>
-          <SectionTitle title={'Sobre mim'}/>
+          <div className='flex flex-col gap-2.5'>
+            <div className="w-4 h-0.5 laptop:w-14 laptop:h-1.5 bg-linear-to-r from-blue-300 to-blue-500" />
+            <SectionTitle title={'Sobre mim'}/>
+          </div>
         </div>
       </Container>
     </section>
