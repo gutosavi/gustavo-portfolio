fa4e66b5e5e5877912f922e957caa18d23a47b0b
Gustavo Savi <gutosavi@hotmail.com>
2026-05-22 21:26:10 -0300

    feat: cria componente MenuMobile.tsx

---
+++ src/components/MenuMobile.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+interface MenuMobileProp {
+  isOpen: boolean;
+}
+
+const MenuMobile = ({ isOpen }: MenuMobileProp) => {
+  return (
+    <nav className={`
+            absolute
+            top-full
+            left-1/2
+            -translate-x-1/2
+            z-50
+            w-full
+            
+            laptop:hidden
+            overflow-hidden
+            transition-all
+            duration-500
+            ease-in-out
+
+            backdrop-blur-md
+            rounded-2xl
+            shadow-xl
+
+            ${isOpen
+              ? "max-h-96 opacity-100 mt-8 p-6"
+              : "max-h-0 opacity-0 p-0"}
+          `}>
+          <ul className="flex flex-col items-center gap-6 text-2xl">
+            <li>
+              <a className="animation-menu text-primary-900 font-display" href="#sobre">
+                Sobre
+              </a>
+            </li>
+
+            <li>
+              <a className="animation-menu text-primary-900 font-display" href="#projetos">
+                Projetos
+              </a>
+            </li>
+
+            <li>
+              <a className="animation-menu text-primary-900 font-display" href="#skills">
+                Skills
+              </a>
+            </li>
+
+            <li>
+              <a className="animation-menu text-primary-900 font-display" href="#contato">
+                Contato
+              </a>
+            </li>
+          </ul>
+        </nav>
+  )
+}
+
+export default MenuMobile
