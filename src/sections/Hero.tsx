190178da904deb28ae5481b6eaab8a25017424c6
Gustavo Savi <gutosavi@hotmail.com>
2026-05-26 08:53:36 -0300

    fix: ajusta bug de responsividade layout

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -6,12 +6,12 @@ import Decoration from '../components/Decoration';
 
 const Hero = () => {
   return (
-    <section>
+    <section className='section'>
       <Container>
         <div className='layout-grid animate-show-left'>
           <img className="hidden laptop:block laptop:col-start-1 laptop:col-end-7 w-full h-full object-cover left-0 top-0 rounded-lg" src={HeroImg} />
             
-          <div className='col-span-full mx-auto max-w-96 pl-5
+          <div className='col-span-full mx-auto max-w-96 pl-5 pb-5
           laptop:pl-0
           laptop:row-start-1 laptop:col-end-13 laptop:col-span-5 flex flex-col gap-6'>
             <div className='laptop:mt-20'>
