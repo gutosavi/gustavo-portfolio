f352d706e6eea59f64788c44564cc364f2c4038d
Gustavo Savi <gutosavi@hotmail.com>
2026-05-27 15:01:57 -0300

    feat: adiciona animação aos cards

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -6,6 +6,8 @@ import Container from '../components/Container'
 import SectionTitle from '../components/SectionTitle'
 import Section from '../components/Section'
 import AnimationCard from '../components/AnimationCard'
+import { decorativeCardAnimation } from '../animations/animations'
+import { motion } from 'framer-motion'
 
 const Skills = () => {
   return (
@@ -18,37 +20,47 @@ const Skills = () => {
               <SectionTitle title={'Skills'} style={{ width: "240px" }}/>
             </div>
 
-            <div className='flex flex-col gap-7 p-1 *:bg-blue-100 *:shadow-[0px_0px_4px_0px_rgba(0,0,0,0.25)] laptop:col-span-full overflow-hidden laptop:col-start-8'>
+            <div className='flex flex-col gap-7 p-1 *:bg-blue-100 *:shadow-[0px_0px_4px_0px_rgba(0,0,0,0.25)] laptop:col-span-full overflow-hidden laptop:col-start-8'>      
               <AnimationCard classe={'relative p-5 rounded-2xl'} delay={0.2}>
-                <div 
+                
+                <motion.div
+                  variants={decorativeCardAnimation} 
                   className='absolute top-7 left-0 w-1 h-8 bg-linear-to-b
                 from-primary-300
                 to-primary-500'>
-                </div>
+                </motion.div>
                 <img src={CodeIcon} className='w-16 h-12 text-primary-900 mb-3.5'/>
                 <h2 className='text-primary-900 mb-2.5 font-display text-2xl font-bold leading-8'>Linguagens</h2>
                 <p className='text-black text-2xl '>JavaScript, TypeScript, React, Python</p>
+              
               </AnimationCard>
               <AnimationCard classe={'relative p-5 rounded-2xl'} delay={0.4}>
-                <div 
+                
+                <motion.div
+                  variants={decorativeCardAnimation} 
                   className='absolute top-7 left-0 w-1 h-8 bg-linear-to-b
                 from-primary-300
                 to-primary-500'>
-                </div>
+                </motion.div>
                 <img src={DataStorageIcon} className='w-16 h-12 text-primary-900 mb-3.5'/>
                 <h2 className='text-primary-900 mb-2.5 font-display text-2xl font-bold leading-8'>Databases</h2>
                 <p className='text-black text-2xl '>SQL, PostgreSQL</p>
+              
               </AnimationCard>
               <AnimationCard classe={'relative p-5 rounded-2xl'} delay={0.6}>
-                <div 
+                
+                <motion.div
+                  variants={decorativeCardAnimation} 
                   className='absolute top-7 left-0 w-1 h-8 bg-linear-to-b
                 from-primary-300
                 to-primary-500'>
-                </div>
+                </motion.div>
                 <img src={ServerIcon} className='w-16 h-12 text-primary-900 mb-3.5'/>
                 <h2 className='text-primary-900 mb-2.5 font-display text-2xl font-bold leading-8'>Server & APIs</h2>
                 <p className='text-black text-2xl '>Node.js, Rest APIs</p>
+              
               </AnimationCard>
+            
             </div>
           </div>
         </Container>
