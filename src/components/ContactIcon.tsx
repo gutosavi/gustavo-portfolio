6acfb2d25471faa6d3b12d54caac10e3b6eafc14
Gustavo Savi <gutosavi@hotmail.com>
2026-05-27 16:39:16 -0300

    feat: cria componente reutilizável

---
+++ src/components/ContactIcon.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import { cardAnimation } from '../animations/animations';
+
+type ContactIconProp = {
+  name: string;
+  contact: string;
+  icon: string;
+  href?: string;
+  delay?: number;
+}
+
+const ContactIcon = ({ name, contact, icon, href, delay = 0 }: ContactIconProp) => {
+  return (
+    <>
+      <img
+        src={icon}
+        alt={name}
+      />
+      <div>
+        <h3 className='text-primary-900 text-xl font-bold font-display uppercase'>{name}</h3>
+        <p className='text-primary-500 text-xl font-normal font-display'>
+          <motion.a 
+          variants={cardAnimation}
+          custom={delay}
+          initial="hidden"
+          whileInView="visible"
+          whileHover="hover"
+          viewport={{ once: true, amount: 0.3 }}
+          href={href}
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="
+            group
+            flex
+            items-center
+            gap-4
+          "
+          >{contact}
+          </motion.a>
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default ContactIcon
