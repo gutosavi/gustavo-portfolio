673e8312aa93618daa0f16c418f870130d10670a
Gustavo Savi <gutosavi@hotmail.com>
2026-05-28 09:18:13 -0300

    refactor: deleta imports inutilizados

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,5 @@
 import React from 'react'
-import Section from './Section'
 import { FaLinkedin, FaGithub, FaInstagram } from 'react-icons/fa'
-import LinkedinIconFooter from '../assets/image/linkedin-icon-footer.svg'
-import GithubIcon from '../assets/image/github-icon.svg'
-import InstagramIcon from '../assets/image/instagram-icon.svg'
 import Container from './Container'
 
 const Footer = () => {
