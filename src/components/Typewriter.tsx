eb782fc97c84b8a7c7858e05b793750bffe2eab8
Gustavo Savi <gutosavi@hotmail.com>
2026-05-27 21:49:22 -0300

    refactor: ajusta lógica do typing e implementa IntersectObserver

--- src/components/Typewriter.tsx
+++ src/components/Typewriter.tsx
@@ -2,9 +2,29 @@ import React from 'react';
 
 const Typewriter = ({ text, className, speed = 50 }: TypewriterProps) => {
   const [displayedText, setDisplayedText] = React.useState('');
+  const currentViewPort = React.useRef(null);
+  const [hasStarted, setHasStarted] = React.useState(false);
 
   React.useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) setHasStarted(true);
+        observer.disconnect();
+      });
+    });
+    if (currentViewPort.current) {
+      observer.observe(currentViewPort.current);
+    }
+    
+    return () => observer.disconnect();
+  }, []);
+  
+  React.useEffect(() => {
+    if (!hasStarted) return;
+    
+    setDisplayedText('')
     let i = 0;
+
     const typingInterval = setInterval(() => {
       if (i < text.length) {
         setDisplayedText(prev => prev + text.charAt(i));
@@ -12,14 +32,17 @@ const Typewriter = ({ text, className, speed = 50 }: TypewriterProps) => {
       } else {
         clearInterval(typingInterval);
       }
+      console.log(i);
     }, speed);
-
+    
     return () => clearInterval(typingInterval);
-  }, [text, speed]);
+  }, [hasStarted, text, speed]);
+
 
   return (
-    <h2 className={className}>
+    <h2 ref={currentViewPort} className={className}>
       {displayedText}
+      <span className="text-neutral-900/50 animate-blink">|</span>
     </h2>
   )
 }
