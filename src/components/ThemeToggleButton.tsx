be838e090e197164a543c504750f4f8caa92a36e
Gustavo Savi <gutosavi@hotmail.com>
2026-06-01 18:31:26 -0300

    feat: implementa animação toggle theme

--- src/components/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton.tsx
@@ -1,28 +1,16 @@
 import React from 'react';
 import { useDarkMode } from '../hooks/useDarkMode';
-import { CgSun } from 'react-icons/cg';
-import { MdDarkMode } from 'react-icons/md';
+import { Classic } from './ClassicThemeToggle';
 
 const ThemeToggleButton = () => {
   const [isDark, setIsDark] = useDarkMode();
 
-  React.useEffect(() => {});
-
   return (
-    <button
-      className="cursor-pointer"
+    <Classic
+      className="cursor-pointer text-primary-900 dark:text-primary-50 text-2xl"
       aria-label="toggle theme"
       onClick={() => setIsDark(!isDark)}
-    >
-      {isDark ? (
-        <CgSun className="text-primary-900 dark:text-primary-50" size={15} />
-      ) : (
-        <MdDarkMode
-          className="text-primary-900 dark:text-primary-50"
-          size={15}
-        />
-      )}
-    </button>
+    ></Classic>
   );
 };
 
