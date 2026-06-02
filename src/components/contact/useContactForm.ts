7b2d0f1850a3bff7ab5f851affe49759754352de
Gustavo Savi <gutosavi@hotmail.com>
2026-06-02 20:00:18 -0300

    feat: implementação de formulário em desenvolvimento

--- src/components/contact/useContactForm.ts
+++ src/components/contact/useContactForm.ts
@@ -9,9 +9,38 @@ type ContactFormData = {
 
 const useContactForm = () => {
   const methods = useForm<ContactFormData>();
+  const [isLoading, setIsLoading] = React.useState(false);
 
-  const onSubmit = (data: ContactFormData) => {
-    console.log(data);
+  const onSubmit = async (data: ContactFormData) => {
+    const formData = {
+      service_id: 'service_5mgfw1j',
+      template_id: 'template_dgdobn4',
+      user_id: 'MHm2pKKkCn2lMjZAN',
+      template_params: {
+        from_name: `${data.name}`,
+        reply_to: `${data.email}`,
+        message: `${data.message}`,
+      },
+    };
+
+    try {
+      const response = await fetch(
+        'https://api.emailjs.com/api/v1.0/email/send',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+      if (!response.ok) {
+        throw new Error('A mensagem não pôde ser enviada!');
+      }
+      console.log('Mensagem enviada com sucesso!');
+    } catch (error) {
+      console.error('Ooops:', error);
+    }
   };
 
   return {
