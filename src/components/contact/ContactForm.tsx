c8d9ab776082757139b9445703c0d64990833eea
Gustavo Savi <gutosavi@hotmail.com>
2026-06-02 21:11:37 -0300

    feat: implementação de envio de formulário via EMAILJS em desenvolvimento

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -6,48 +6,54 @@ import Button from '../ui/Button';
 import TextArea from '../ui/TextArea';
 
 const ContactForm = () => {
-  const { methods, onSubmit } = useContactForm();
+  const { methods, isLoading, onSubmit } = useContactForm();
 
   return (
-    <FormProvider {...methods}>
-      <form
-        onSubmit={methods.handleSubmit(onSubmit)}
-        className="flex flex-col w-full pt-15 pb-10"
-      >
-        <h2 className="text-primary-900 dark:text-primary-50 text-3xl font-bold font-display leading-8">
-          Ou, se preferir, envie-me uma mensagem:
-        </h2>
-        <Input
-          name="name"
-          placeholder="Nome"
-          validation={{
-            required: 'Nome é obrigatório',
-            pattern: {
-              value: /^[A-Za-zÀ-ÖØ-öø-ÿ\s'-]+$/,
-              message: 'Nome é inválido',
-            },
-          }}
-        />
-        <Input
-          name="email"
-          placeholder="Endereço de e-mail"
-          validation={{
-            required: 'E-mail é obrigatório',
-            pattern: {
-              value: /^[^@\s]+@[^@\s]+\.[^@\s]+$/,
-              message: 'E-mail é inválido',
-            },
-          }}
-        />
-        <TextArea
-          name="message"
-          placeholder="Me fale um pouco sobre seu projeto"
-          rows={10}
-          cols={30}
-        />
-        <Button name="Enviar" />
-      </form>
-    </FormProvider>
+    <>
+      {isLoading ? (
+        <span>Enviando...</span>
+      ) : (
+        <FormProvider {...methods}>
+          <form
+            onSubmit={methods.handleSubmit(onSubmit)}
+            className="flex flex-col w-full pt-15 pb-10"
+          >
+            <h2 className="text-primary-900 dark:text-primary-50 text-3xl font-bold font-display leading-8">
+              Ou, se preferir, envie-me uma mensagem:
+            </h2>
+            <Input
+              name="name"
+              placeholder="Nome"
+              validation={{
+                required: 'Nome é obrigatório',
+                pattern: {
+                  value: /^[A-Za-zÀ-ÖØ-öø-ÿ\s'-]+$/,
+                  message: 'Nome é inválido',
+                },
+              }}
+            />
+            <Input
+              name="email"
+              placeholder="Endereço de e-mail"
+              validation={{
+                required: 'E-mail é obrigatório',
+                pattern: {
+                  value: /^[^@\s]+@[^@\s]+\.[^@\s]+$/,
+                  message: 'E-mail é inválido',
+                },
+              }}
+            />
+            <TextArea
+              name="message"
+              placeholder="Me fale um pouco sobre seu projeto"
+              rows={10}
+              cols={30}
+            />
+            <Button name="Enviar" />
+          </form>
+        </FormProvider>
+      )}
+    </>
   );
 };
 
--- src/components/contact/useContactForm.ts
+++ src/components/contact/useContactForm.ts
@@ -1,50 +1,27 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-
-type ContactFormData = {
-  name: string;
-  email: string;
-  message: string;
-};
+import { postData } from '../../services/postData';
 
 const useContactForm = () => {
   const methods = useForm<ContactFormData>();
   const [isLoading, setIsLoading] = React.useState(false);
 
   const onSubmit = async (data: ContactFormData) => {
-    const formData = {
-      service_id: 'service_5mgfw1j',
-      template_id: 'template_dgdobn4',
-      user_id: 'MHm2pKKkCn2lMjZAN',
-      template_params: {
-        from_name: `${data.name}`,
-        reply_to: `${data.email}`,
-        message: `${data.message}`,
-      },
-    };
-
+    setIsLoading(true);
     try {
-      const response = await fetch(
-        'https://api.emailjs.com/api/v1.0/email/send',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(formData),
-        }
-      );
-      if (!response.ok) {
-        throw new Error('A mensagem não pôde ser enviada!');
-      }
-      console.log('Mensagem enviada com sucesso!');
+      const dataForm = await postData(data);
+      methods.reset();
+      return dataForm;
     } catch (error) {
-      console.error('Ooops:', error);
+      console.error('Erro ao enviar mensagem', error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return {
     methods,
+    isLoading,
     onSubmit,
   };
 };
